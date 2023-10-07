33baca01c910a0552921f6a0816d5bd8083a6d94
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2023-10-08 01:09:24 +0530

    Learning Redux , react-redux and reductoolkit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { AddTodo, Todo } from "./components";
+
+function App() {
+  return (
+    <>
+      <h1>Todo App</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
